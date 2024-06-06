713b867efdaf5b6cc79fcb1c334b1db8939ae1bb
indiakka <78439130+indiakka@user.noreply.github.com>
2024-06-06 11:40:37 +0200

    fetch de api

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const jokeEl = document.('')
-const jokeBtn = document.('')
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-jokeBtn.addEventListener('', generateJoke)
+jokeBtn.addEventListener("click", generateJoke);
+
+const url = "https://icanhazdadjoke.com";
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
-      Accept: '',
+      Accept: "application/json",
     },
-  }
+  };
 
-  const res = await ('', config)
+  const res = await fetch((url), config);
 
-  const data = await res.json()
+  const data = await res.json();
 
-  jokeEl.innerHTML = data.joke
+  jokeEl.innerHTML = data.joke;
 }
 
 //   HINTS
